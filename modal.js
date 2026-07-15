ee82a88cf325f3c934db3a47ec1bcf95afd94355
Mveli01 <107203688+Mveli01@users.noreply.github.com>
2026-07-15 15:10:32 +0200

    Updated files

--- modal.js
+++ modal.js
@@ -2,18 +2,27 @@ const modalWrapper = document.getElementById('modal-wrapper');
 const modalContainer = document.getElementById('modal-container');
 
 export default function showModal(product) {
-    modalContainer.innerHTML= `<div>
-                <img src="${product.image}" width="150">
-                <h3>${product.name}</h3>
-                <p>R ${(product.priceCents / 100).toFixed(2)}</p>
-                <h4>${product.subCategory}</h4>
-                <p>Rating: ${product.rating.stars}</p>
-                <h5>Description: ${product.description}</h5>
-                <p>In Stock: ${product.rating.count} items</p>
-                <button id="add-to-cart" data-add="${product.id}">Add to Cart</button>
-                <button id="close-modal" data-close="close">Close</button>
-                </div>
-                `
+    modalContainer.innerHTML = `
+    <div class="modal-content">
+        <img src="${product.image}" class="modal-image">
+
+        <div class="modal-details">
+            <h3>${product.name}</h3>
+            <p>R ${(product.priceCents / 100).toFixed(2)}</p>
+            <h4>${product.subCategory}</h4>
+            <p>Rating: ${product.rating.stars}</p>
+            <p><strong>Description:</strong> ${product.description}</p>
+            <p>In Stock: ${product.rating.count} items</p>
+
+            <button id="add-to-cart" data-add="${product.id}">
+                Add to Cart
+            </button>
+
+            <button id="close-modal" data-close="close">
+                Close
+            </button>
+        </div>
+    </div>`
                
         modalWrapper.style.display = 'flex';
         
--- renderProducts.js
+++ renderProducts.js
@@ -5,8 +5,8 @@ export default function renderProducts(products, productContainer){
 
     products.forEach(product => {
         html += `
-            <div>
-                <img src="${product.image}" width="150">
+            <div class="product-card">
+                <img src="${product.image}" class="product-image">
                 <h2>${product.name}</h2>
                 <p>R ${(product.priceCents / 100).toFixed(2)}</p>
                 <button id="view-details" data-view="${product.id}">View details</button>
