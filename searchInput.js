76544a97f93f95268b2746a87e64873c835d3edf
Mveli01 <107203688+Mveli01@users.noreply.github.com>
2026-08-19 14:38:22 +0200

    Added comments

--- searchInput.js
+++ searchInput.js
@@ -1,6 +1,10 @@
 const searchInput = document.getElementById('search-input');
 import renderProducts from './renderProducts.js';
 
+/*
+    A function to search for products based on user input
+*/
+
 export default function searchProducts(products, productContainer, searchInput){
     const searchItem = searchInput.value.toLowerCase();
      const filteredProducts = products.filter(product => 
